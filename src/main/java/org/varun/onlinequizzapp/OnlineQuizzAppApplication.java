fb9b0bd102d89cb192886f879c7c2c0b44f203c7
Varun Sai Nadiminti <varunsainadiminti@gmail.com>
2025-11-15 17:07:35 +0530

    Project Initialization

---
+++ src/main/java/org/varun/onlinequizzapp/OnlineQuizzAppApplication.java
@@ -0,0 +1,13 @@
+package org.varun.onlinequizzapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineQuizzAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineQuizzAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/varun/onlinequizzapp/OnlineQuizzAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.varun.onlinequizzapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineQuizzAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
