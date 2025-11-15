c8c1f14cc56477ab264e504ad1c6cb408219a646
Varun Sai Nadiminti <varunsainadiminti@gmail.com>
2025-11-15 21:33:15 +0530

    Created models for user, quiz, questions, attempts and answers

--- src/main/java/org/varun/onlinequizzapp/model/Attempt.java
+++ src/main/java/org/varun/onlinequizzapp/model/Attempt.java
@@ -1,4 +1,31 @@
 package org.varun.onlinequizzapp.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "attempts")
 public class Attempt {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    @ManyToOne
+    @JoinColumn(name = "quiz_id")
+    private Quiz quiz;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    private int attemptNumber;
+    private int totalScore;
+
+    @OneToMany(mappedBy = "attempt")
+    private List<Answer> answers;
 }
--- src/main/java/org/varun/onlinequizzapp/model/User.java
+++ src/main/java/org/varun/onlinequizzapp/model/User.java
@@ -1,4 +1,43 @@
 package org.varun.onlinequizzapp.model;
 
-public class User {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    @Column(nullable = false, unique = true)
+    private String username;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    private ROLE role;
+
+    private boolean isEnabled;
+    private String verificationCode;
+    private String codeExpiresIn;
+
+    @ManyToMany(mappedBy = "users")
+    private List<Quiz> quizzes;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role.name()));
+    }
 }
