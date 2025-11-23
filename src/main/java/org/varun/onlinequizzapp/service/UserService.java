eecf62e5ea205b548f4cfc042369bbaf10cea10a
Varunsai85 <varunsainadiminti@gmail.com>
2025-11-23 20:26:57 +0530

    Modified User Service

--- src/main/java/org/varun/onlinequizzapp/service/UserService.java
+++ src/main/java/org/varun/onlinequizzapp/service/UserService.java
@@ -1,4 +1,19 @@
 package org.varun.onlinequizzapp.service;
 
-public class UserService {
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.varun.onlinequizzapp.dao.UserRepository;
+
+@Service
+@AllArgsConstructor
+public class UserService implements UserDetailsService {
+    private UserRepository userRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+        return userRepo.findByUsernameOrEmail(login,login).orElseThrow(()->new UsernameNotFoundException("User not found with "+login));
+    }
 }
