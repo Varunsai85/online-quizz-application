db1b66fbd7a07c0d584c38f746a14d479b1023c6
Varunsai85 <varunsainadiminti@gmail.com>
2025-11-24 11:54:46 +0530

    Defined Dto for signUp and Api Response

--- src/main/java/org/varun/onlinequizzapp/dto/ApiResponse.java
+++ src/main/java/org/varun/onlinequizzapp/dto/ApiResponse.java
@@ -1,4 +1,20 @@
 package org.varun.onlinequizzapp.dto;
 
-public class ApiResponse {
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Data
+public class ApiResponse<T> {
+    private T data;
+    private String message;
+
+    public ApiResponse(String message, T data) {
+        this.message = message;
+        this.data = data;
+    }
+
+    public ApiResponse(String message) {
+        this.message = message;
+    }
 }
--- src/main/java/org/varun/onlinequizzapp/dto/SignUpDto.java
+++ src/main/java/org/varun/onlinequizzapp/dto/SignUpDto.java
@@ -1,16 +1,16 @@
 package org.varun.onlinequizzapp.dto;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
-public record SignUpRequest(
+public record SignUpDto(
         @NotNull(message = "Username cannot be null")
         String username,
         @NotNull(message = "Email cannot be null")
         @Email
         String email,
-        @Min(value = 3, message = "Password must be at least 6 characters long")
+        @Size(min = 3, message = "Password must be at least 3 characters long")
         String password
 ) {
 }
