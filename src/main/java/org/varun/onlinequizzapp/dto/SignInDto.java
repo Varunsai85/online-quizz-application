813d307479d709a1d478f93e4d906502a8e6012f
Varunsai85 <varunsainadiminti@gmail.com>
2025-11-24 14:30:55 +0530

    Defined signIn and verificaton code dto

--- src/main/java/org/varun/onlinequizzapp/dto/SignInDto.java
+++ src/main/java/org/varun/onlinequizzapp/dto/SignInDto.java
@@ -1,4 +1,12 @@
 package org.varun.onlinequizzapp.dto;
 
-public record SignInDto() {
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record SignInDto(
+        @NotNull(message = "Username cannot be null")
+        String login,
+        @Size(min = 3, message = "Password must be at least 3 characters long")
+        String password
+) {
 }
--- src/main/java/org/varun/onlinequizzapp/dto/VerificationCodeDto.java
+++ src/main/java/org/varun/onlinequizzapp/dto/VerificationCodeDto.java
@@ -1,4 +1,13 @@
 package org.varun.onlinequizzapp.dto;
 
-public record VeificationCodeDto() {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+
+public record VerificationCodeDto(
+        @NotNull(message = "Email is required")
+        @Email(message = "Invalid email format")
+        String email,
+        @NotNull(message = "Code is required")
+        String code
+) {
 }
