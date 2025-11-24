062e12f25ceaf6db4cfe5ae6a5df4209bd2ca795
Varunsai85 <varunsainadiminti@gmail.com>
2025-11-24 14:30:12 +0530

    Created sign-in, verify and resend endpoints

--- src/main/java/org/varun/onlinequizzapp/controller/AuthController.java
+++ src/main/java/org/varun/onlinequizzapp/controller/AuthController.java
@@ -3,11 +3,10 @@ package org.varun.onlinequizzapp.controller;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.varun.onlinequizzapp.dto.SignInDto;
 import org.varun.onlinequizzapp.dto.SignUpDto;
+import org.varun.onlinequizzapp.dto.VerificationCodeDto;
 import org.varun.onlinequizzapp.service.AuthService;
 
 @RestController
@@ -19,5 +18,20 @@ public class AuthController {
     @PostMapping("/sign-up")
     public ResponseEntity<?> sigUp(@Valid @RequestBody SignUpDto signUpDto){
         return authService.signUp(signUpDto);
+    }
+
+    @PostMapping("/sign-in")
+    public ResponseEntity<?> signIn(@Valid @RequestBody SignInDto signInDto){
+        return authService.signIn(signInDto);
+    }
+
+    @PostMapping("/verify")
+    public ResponseEntity<?> verify(@Valid @RequestBody VerificationCodeDto verificationCodeDto){
+        return authService.verifyCode(verificationCodeDto);
+    }
+
+    @GetMapping("/resend-mail")
+    public ResponseEntity<?> resendEmail(@RequestParam String email){
+        return authService.resendVerificationEmail(email);
     }
 }
