2ea48daf67f28885a4c188afeb1a8eab3e53a448
Varunsai85 <varunsainadiminti@gmail.com>
2025-11-26 16:16:37 +0530

    Implemented Global exception handler primarity for validation errors

--- src/main/java/org/varun/onlinequizzapp/exception/GlobalExceptionHandler.java
+++ src/main/java/org/varun/onlinequizzapp/exception/GlobalExceptionHandler.java
@@ -1,4 +1,24 @@
 package org.varun.onlinequizzapp.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.varun.onlinequizzapp.dto.ApiResponse;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleValidationErrors(MethodArgumentNotValidException ex){
+        String errMessage=ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error-> error.getField()+":"+error.getDefaultMessage())
+                .findFirst()
+                .orElse("Validation Failed");
+
+        return new ResponseEntity<>(new ApiResponse<>(errMessage), HttpStatus.BAD_REQUEST);
+    }
 }
