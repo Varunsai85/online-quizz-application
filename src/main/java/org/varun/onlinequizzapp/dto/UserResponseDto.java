990598c8bb35dff6c9afb3503db7c9f851c7ee2d
Varunsai85 <varunsainadiminti@gmail.com>
2025-12-02 09:33:37 +0530

    Modified user response dto to give infor about user verification

--- src/main/java/org/varun/onlinequizzapp/dto/UserResponseDto.java
+++ src/main/java/org/varun/onlinequizzapp/dto/UserResponseDto.java
@@ -5,6 +5,7 @@ import org.varun.onlinequizzapp.model.type.Role;
 public record UserResponseDto(
         String username,
         String email,
-        Role role
+        Role role,
+        boolean isEnabled
 ) {
 }
