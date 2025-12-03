be0342119e949e36334f383d4d1356c6cc6bc417
Varunsai85 <varunsainadiminti@gmail.com>
2025-12-03 14:04:38 +0530

    Updated @NotNull validation for password

--- src/main/java/org/varun/onlinequizzapp/dto/SignInDto.java
+++ src/main/java/org/varun/onlinequizzapp/dto/SignInDto.java
@@ -4,8 +4,9 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record SignInDto(
-        @NotNull(message = "Username cannot be null")
+        @NotNull(message = "Username or Email is required")
         String login,
+        @NotNull(message = "Password is required")
         @Size(min = 3, message = "Password must be at least 3 characters long")
         String password
 ) {
--- src/main/java/org/varun/onlinequizzapp/dto/SignUpDto.java
+++ src/main/java/org/varun/onlinequizzapp/dto/SignUpDto.java
@@ -5,11 +5,12 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record SignUpDto(
-        @NotNull(message = "Username cannot be null")
+        @NotNull(message = "Username is required")
         String username,
-        @NotNull(message = "Email cannot be null")
+        @NotNull(message = "Email is required")
         @Email
         String email,
+        @NotNull(message = "Password is required")
         @Size(min = 3, message = "Password must be at least 3 characters long")
         String password
 ) {
